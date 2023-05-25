49880d706d09f6a01cb49f99b446a9f452e2ab5d
Karam Almakdasi <karamalmakdasi@gmail.com>
2023-05-25 10:43:53 +0200

    #-skapdae en ny project portfolioAPI i samma solustion

---
+++ portfoljoAPI/Model/PortfolioAPI.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace portfoljoAPI.Model
+{
+    public class PortfolioAPI
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
